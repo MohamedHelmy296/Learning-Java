d20b8265171f062b3f48699556f4db80db11e2c0
Mohamed Helmy <142120424+MohamedHelmy296@users.noreply.github.com>
2023-11-14 17:33:26 +0200

    Create program_1.java

---
+++ program_1.java
@@ -0,0 +1,18 @@
+//to get input from user
+import java.util.Scanner;
+public class Newclass
+{
+    //This is the entry point of the program
+    public static void main (String[]args)
+    {
+        //accrpt input from user
+        Scanner input = new Scanner(System.in);
+        int length , width , area ;
+        System.out.println("Enter rectangle length");
+        length = input.nextInt();
+        System.out.println("Enter rectangle Width");
+        width = input.nextInt();
+        area = length * width ;
+        System.out.print("area = " + area);
+    }
+}
