fc2808e93866f3db306bf7e52502e5376312ae26
Mohamed Helmy <142120424+MohamedHelmy296@users.noreply.github.com>
2023-12-01 16:55:37 +0200

    Create 3- repetition_Control_Statement.java

---
+++ Java Basic /3- repetition_Control_Statement.java
@@ -0,0 +1,23 @@
+------------------------------------- while loop ----------------
+intial value here counter ; 
+while ( ...) // boolean expretion (true)   if became faluse stop  >> condition
+  {
+
+    update counter ; 
+  }
+
+------------------------------------ do while loop ------------------ 
+  
+at least one 
+intial value here counter ; 
+do{
+update counter ;
+}while (..); // boolean expretion (true)   if became faluse stop  >> condition
+
+------------------------------------ for loop ----------------------------
+
+for ( ; ; )
+  {
+    
+  }
+--------------------------------------------------------------------------
