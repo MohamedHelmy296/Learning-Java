3627aacc7175dee25ea47cf3aef4b5b9bc3723fb
Mohamed Helmy <142120424+MohamedHelmy296@users.noreply.github.com>
2023-12-01 16:29:29 +0200

    Create 2- selection_statement.java

---
+++ Java Basic /2- selection_statement.java
@@ -0,0 +1,44 @@
+(selection statement)
+
+-------------------------if statement-------------------------------------------------
+if (....) {
+}
+--------------------------if else statement---------------------------------------------------
+if (....) { 
+}
+else{
+}
+--------------------------if else if statement--------------------------------------------------
+if (....) { 
+}
+else if (...){
+}
+else{
+}
+--------------------------nested if statement--------------------------------------------------
+if (....) {
+if(....){
+}
+}
+else{
+}
+--------------------------- ternary operator ---------------------------------------------
+?: 
+x = y==1?2:5
+means
+if (y ==2)
+x = 1
+else
+x = 5
+------------------------- instance of -----------------------------------------------------
+r = instanceof string  >>>>>>>>> true or false 
+بسال علي المتغير دا من نوع كذا ؟ 
+if the variable is an object from this type of not 
+ use only with  primitive data type 
+ ---------------------------------Switch statement--------------------------
+ switch(..){              //byte, short, char, int primitive data types
+ case..:
+ break ; 
+
+
+ default: 
