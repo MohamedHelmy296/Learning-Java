c417d1e6b0120b4f28a4a31ce6154a5ab42a4d37
Mohamed Helmy <142120424+MohamedHelmy296@users.noreply.github.com>
2023-12-02 17:08:09 +0200

    Update 5- Methods.java

--- Java Basic /5- Methods.java
+++ Java Basic /5- Methods.java
@@ -4,23 +4,32 @@
   1- return value type 
   2- name of method (function) 
   3- calling in main 
-  any code write in class !! and methods write in class above the main 
+  any code written in class !! and methods written in class above the main 
  ----------------------------------------------------
-*static >>>>> to call it without create object 
+* static >>>>> to call it without creating an object 
  ----------------------------------------------------
-*public >>>>> to call it in any file in the package(project)
+* public >>>>> to call it in any file in the package(project)
  ----------------------------------------------------
 
   // variable number of arguments 
   public static int num (int...n){ // n is array 
   }
-  >>>>>>>>>     in main you can call the method 'num' and pass any number of parameters 
+  >>>>>>>>>     In main you can call the method 'num' and pass any number of parameters 
   
   ---------------------------------------------------- look test13 
 
   #Built-in function (Methods) 
   1-> Java.Number.Methods ( https://docs.oracle.com/javase/8/docs/api/java/lang/Math.html )
+                           like math >> import static java.lang.Math.*;          // * to call all methods
+
   2-> Java.String.Methods ( https://docs.oracle.com/javase/8/docs/api/java/lang/String.html )
   3-> Java.Character.Methods ( https://docs.oracle.com/javase/8/docs/api/java/lang/Character.html )
   4-> Java.Array.Methods (
-  
+
+  --------------------------------------------------------
+    /* Method Overloading
+* Overloading allows different methods to have the same name,
+* bur different signatures where the signature can differ by 
+* the number of input parameters or type of input parameters or both.
+* Overloading is related to compile-time (or static) polymorphism. */
+    
