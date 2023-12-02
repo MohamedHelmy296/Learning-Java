701c3a679e66f65736762b0bb5202feab1ddaa75
Mohamed Helmy <142120424+MohamedHelmy296@users.noreply.github.com>
2023-12-02 16:46:48 +0200

    Create 5- Methods.java

---
+++ Java Basic /5- Methods.java
@@ -0,0 +1,26 @@
+* Reusability 
+  ----------------------------------------------------
+  methods consist of :
+  1- return value type 
+  2- name of method (function) 
+  3- calling in main 
+  any code write in class !! and methods write in class above the main 
+ ----------------------------------------------------
+*static >>>>> to call it without create object 
+ ----------------------------------------------------
+*public >>>>> to call it in any file in the package(project)
+ ----------------------------------------------------
+
+  // variable number of arguments 
+  public static int num (int...n){ // n is array 
+  }
+  >>>>>>>>>     in main you can call the method 'num' and pass any number of parameters 
+  
+  ---------------------------------------------------- look test13 
+
+  #Built-in function (Methods) 
+  1-> Java.Number.Methods ( https://docs.oracle.com/javase/8/docs/api/java/lang/Math.html )
+  2-> Java.String.Methods ( https://docs.oracle.com/javase/8/docs/api/java/lang/String.html )
+  3-> Java.Character.Methods ( https://docs.oracle.com/javase/8/docs/api/java/lang/Character.html )
+  4-> Java.Array.Methods (
+  
