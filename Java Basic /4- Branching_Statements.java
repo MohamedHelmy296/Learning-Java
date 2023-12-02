1d6ebba73c57a38216f5aeeaaad6115a9b90dd9a
Mohamed Helmy <142120424+MohamedHelmy296@users.noreply.github.com>
2023-12-02 12:22:37 +0200

    Create 4- Branching_Statements.java

---
+++ Java Basic /4- Branching_Statements.java
@@ -0,0 +1,39 @@
+  break statement >>>>> exit from control structure 
+        continue statement >>>>> skip  
+                  return statement >>>>> Exit from current method 
+    ( two forms of break statement - unlabeled and labeled. ) 
+   
+    Labeled break statement is used to terminate the outer loop, the loop should be labeled for it to work. 
+    Here is an example showing java break label statement usage.
+-----------------------------------------------------------------------------------------------------------------------------------
+    package com.journaldev.util;
+
+    public class JavaBreakLabel {
+
+	         public static void main(String[] args) {
+                  		int[][] arr = { { 1, 2 }, { 3, 4 }, { 9, 10 }, { 11, 12 } };
+                  		boolean found = false;
+                  		int row = 0;
+                  		int col = 0;
+                  		// find index of first int greater than 10
+                  		searchint:
+                  
+                  		for (row = 0; row < arr.length; row++) {
+                  			for (col = 0; col < arr[row].length; col++) {
+                  				if (arr[row][col] > 10) {
+                  					found = true;
+                  					// using break label to terminate outer statements
+                  					break searchint;
+                  				}
+                  			}
+                  		}
+                  		if (found)
+                  			System.out.println("First int greater than 10 is found at index: [" + row + "," + col + "]");
+              }
+                  
+}
+-----------------------------------------------------------------------------------------------------------------------------------
+
+
+
+    
